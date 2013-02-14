0d0b9c7cedb61f65ccf1a92f2eff1d509aa755ba
Jan Hoppmann <dattHoppi@googlemail.com>
2013-02-14 10:09:47 +0100

    Database class added

---
+++ DHBW-DB/src/com/dhbw_db/model/DataAccess.java
@@ -0,0 +1,16 @@
+/**
+ * DataAccess.java Created by jhoppmann on 14.02.2013
+ */
+package com.dhbw_db.model;
+
+/**
+ * @author jhoppmann
+ * @version 0.1
+ * @since
+ */
+public class DataAccess {
+
+	public DataAccess() {
+		// TODO do things here
+	}
+}
