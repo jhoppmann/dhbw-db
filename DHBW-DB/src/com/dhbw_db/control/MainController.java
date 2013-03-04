e367663f170e7d5f786f50821eb83d2448261b26
jhoppmann <dattHoppi@googlemail.com>
2013-03-04 20:31:55 +0100

    Added asynchronous execution service

--- DHBW-DB/src/com/dhbw_db/control/MainController.java
+++ DHBW-DB/src/com/dhbw_db/control/MainController.java
@@ -3,6 +3,10 @@
  */
 package com.dhbw_db.control;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+import com.dhbw_db.model.execution.Executor;
 import com.vaadin.ui.UI;
 
 /**
@@ -16,16 +20,42 @@ import com.vaadin.ui.UI;
 public class MainController {
 	private UI application;
 
+	private Executor executor;
+
 	public MainController(UI application) {
 		this.application = application;
+		executor = new Executor(2);
 	}
 
+	/**
+	 * Takes care of the standard ouptut for the program
+	 * 
+	 * @param message The message to show
+	 */
 	public void print(String message) {
+		// TODO change this to info messages in the UI
 		System.out.println(message);
 	}
 
+	/**
+	 * Is responsible for standard error message handling.
+	 * 
+	 * @param message The error message
+	 */
 	public void printError(String message) {
+		// TODO change this to info messages in the UI
 		System.err.println(message);
+	}
+
+	/**
+	 * Forwards a call to the executor service to run.
+	 * 
+	 * @param call The call to execute
+	 * @return Returns the <tt>FutureTask</tt> object for reference and result
+	 *         checking
+	 */
+	public FutureTask<String> execute(Callable<String> call) {
+		return executor.addTask(call);
 	}
 
 }
