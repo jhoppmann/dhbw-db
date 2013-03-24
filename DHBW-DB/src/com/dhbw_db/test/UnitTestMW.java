139f7003911519b34785adcfe987c2599a0c3e98
Moritz Walliser <Moritz Walliser@192.168.2.105>
2013-03-24 11:16:47 +0100

    test

---
+++ DHBW-DB/src/com/dhbw_db/test/UnitTestMW.java
@@ -0,0 +1,35 @@
+/**
+ * UnitTestMW.java Created by Moritz Walliser on 24.03.2013
+ */
+
+package com.dhbw_db.test;
+
+import java.util.Date;
+
+import javax.mail.MessagingException;
+
+import com.dhbw_db.model.mail.EmailSessionBean;
+import com.dhbw_db.model.request.Request;
+
+/**
+ * @author Moritz Walliser
+ * @version 0.1
+ * @since
+ */
+public class UnitTestMW {
+
+	public static void main(String args[]) throws MessagingException {
+
+		int requesterId = 0;
+		int approverId = 0;
+		int notebookId = 0;
+		Date until = new Date();
+		Date start = new Date();
+
+		EmailSessionBean a = new EmailSessionBean();
+		Request r = new Request(requesterId,
+								approverId);
+		a.sendMailRequestStudent("moritz.walliser@1und1.de", "test", "test", r);
+
+	}
+}
